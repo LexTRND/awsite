e6fbfc9fddc0a64e9c515d7a3e8ef432bdcb4bad
LexTRND <a.reese@me.com>
2021-06-08 12:02:32 -0700

    Added alerts

--- components/Services.js
+++ components/Services.js
@@ -1,68 +1,59 @@
-import React, { Component } from 'react';
-import {FaBrush,FaCalendarDay,FaFillDrip,} from 'react-icons/fa';
-import {BsFillHouseDoorFill} from "react-icons/bs"
-import { IconContext } from 'react-icons/lib';
-import Title from './Title';
-
-
-
+import React, { Component } from "react";
+import { FaBrush, FaCalendarDay, FaFillDrip } from "react-icons/fa";
+import { BsFillHouseDoorFill } from "react-icons/bs";
+import { IconContext } from "react-icons/lib";
+import Title from "./Title";
 
 export default class Services extends Component {
-    state={
-        services:[
-            {
-                icon:<BsFillHouseDoorFill/>,
-                title:"Residental",
-                info:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec.'
-            },
+  state = {
+    services: [
+      {
+        icon: <BsFillHouseDoorFill />,
+        title: "Residental",
+        info: "Make your home feel warm and cozy. We do, walls, ceilings, trim, cabinets, built-in fixtures, stair rails, you name it. It'll feel like a place you never want to leave",
+      },
 
-            {
-                icon:<FaCalendarDay/>,
-                title:"Commercial",
-                info:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec.'
-            },
+      {
+        icon: <FaCalendarDay />,
+        title: "Commercial",
+        info: "You only get one first impression. Present a professional image at your business with colorful and crisp paint. We partner with EB Color Consultants to help you decide what colors are best for the results you seek. ",
+      },
 
-            {
-                icon:<FaBrush/>,
-                title:"Interior",
-                info:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec.'
-            },
+      {
+        icon: <FaFillDrip />,
+        title: "Exterior",
+        info: "Protect your home or business while taking your curb appeal to the next level",
+      },
 
-            
+      {
+        icon: <FaBrush />,
+        title: "Speciality",
+        info: "Did we forgot something? Dog house? Power washing? Schedule a consolation, we'll be glad to help",
+      },
+    ],
+  };
 
-            {
-                icon:<FaFillDrip/>,
-                title:"Exterior",
-                info:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec.'
-            },
-
-          
-            
-        ]
-    };
-    
-    render() {
-        return (
-            
-            <section className="services">
-
-                <Title title="services" />
-                <div className="services-center">
-
-                   {this.state.services.map((item, index) => {
-                        return (
-                            <IconContext.Provider key={index} value={{ size:'1.5em', className: 'react-icons-service' }}>
-                        <article key={index} className="services">
-                            <span>{item.icon}</span>
-                            <h6>{item.title}</h6>
-                            <p>{item.info}</p>
-                           </article>
-                           </IconContext.Provider>
-                        );
-                   }) }
-                </div>
-            </section>
-        )
-                }
-
-            }
+  render() {
+    return (
+      <section className="services">
+        <Title title="services" />
+        <div className="services-center">
+          {this.state.services.map((item, index) => {
+            return (
+              <IconContext.Provider
+                key={index}
+                value={{ size: "1.5em", className: "react-icons-service" }}
+              >
+                <article key={index} className="services">
+                  <span>{item.icon}</span>
+                  <h6>{item.title}</h6>
+                  <p>{item.info}</p>
+                </article>
+              </IconContext.Provider>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+}
